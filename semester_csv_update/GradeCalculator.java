e31d8a1fc097cc4e531fdfe757927fb457f4bf7f
SoyunJung <163826084+SoyunJung@users.noreply.github.com>
2024-06-16 19:43:55 +0900

    [Feature] #5 - 성적계산기 탭 추가(subjectgrade분리)

--- semester_csv_update/GradeCalculator.java
+++ semester_csv_update/GradeCalculator.java
@@ -321,28 +321,3 @@ public class GradeCalculator extends JPanel {
         }
     }
 }
-
-
-class SubjectGrade {
-    private String subjectName;
-    private int credit;
-    private String grade;
-
-    public SubjectGrade(String subjectName, int credit, String grade) {
-        this.subjectName = subjectName;
-        this.credit = credit;
-        this.grade = grade;
-    }
-
-    public String getSubjectName() {
-        return subjectName;
-    }
-
-    public int getCredit() {
-        return credit;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-}
